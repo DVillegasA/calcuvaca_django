d3a6da1f3a7a2f290f414ef1f7f8782e83732cf0
Diego Villegas Arenas <diego.villegas.12@sansano.usm.cl>
2025-04-28 22:12:07 -0400

    Primera implementación de las vistas
    Es solo un hola mundo. Se tiene que partir por algo.

--- calcuvaca_site/views.py
+++ calcuvaca_site/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
+def home(request: HttpRequest):
+    context = {'message': 'Hola, Mundo!'}
+
+    return render(request, 'home.html', context)
